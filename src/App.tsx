3806cbf037e3b390001142ffbe0edc8c9604ce60
Frit027 <Frit_027@mail.ru>
2024-09-18 19:24:48 +0300

    added controls with buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import { ButtonsControl } from './components/buttons-control';
+import { observer } from 'mobx-react-lite';
+import { Store } from './store';
 
-function App() {
-  return <div></div>;
-}
+const App = observer(({ store1, store2 }: { store1: Store, store2: Store }) => {
+  return <div>
+    <ButtonsControl
+      store={store1}
+      rightButtons={[
+        { title: 'Очистить', callback: store1.clear },
+        { title: 'Установить Hello world!', callback: () => store1.setValue('Hello world!') },
+      ]} />
+    <ButtonsControl
+      store={store2}
+      leftButtons={[{ title: 'alert, если число', callback: store2.alertIfNumber }]}
+      rightButtons={[{ title: 'alert', callback: store2.alertValue }]} />
+  </div>;
+});
 
 export default App;
