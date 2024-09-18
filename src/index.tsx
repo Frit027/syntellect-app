12dad0b88a93a9583b3415f6d35c2a7c5385ba1a
Frit027 <Frit_027@mail.ru>
2024-09-18 20:45:41 +0300

    removed stores

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { store1, store2 } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App store1={store1} store2={store2} />
+    <App/>
   </React.StrictMode>
 );
