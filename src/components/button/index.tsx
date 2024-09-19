736c16457e002bea814d87e4dd899bd2ee1f2beb
Frit027 <Frit_027@mail.ru>
2024-09-19 20:59:06 +0300

    added styles

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,8 +1,13 @@
+import React from 'react';
 import { TProps } from './interfaces';
+import { classes } from './constants';
+import './index.css';
 
 /**
  * Компонент, представляющий собой кнопку
  *
  * @component
  */
-export const Button = ({ title, handleClick }: TProps) => <button onClick={handleClick} type='button'>{title}</button>;
+export const Button = ({ title, handleClick }: TProps) => (
+  <button className={classes.component} onClick={handleClick} type='button'>{title}</button>
+);
