a46ff4aa1f6dfb0b71e273cda990920f74188ef4
Frit027 <Frit_027@mail.ru>
2024-09-19 20:44:15 +0300

    CSS-class names for autocomplete input

---
+++ src/components/autocomplete-input-control/constants.ts
@@ -0,0 +1,8 @@
+export const classes = {
+  component: 'autocomplete-input',
+  list: 'autocomplete-input__list',
+  input: 'autocomplete-input__input',
+  item: 'autocomplete-input__item',
+  text: 'autocomplete-input__text',
+  flag: 'autocomplete-input__flag',
+};
