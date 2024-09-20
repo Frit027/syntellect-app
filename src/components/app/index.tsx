e0590fd6cf385e5be3d22e79a2dafc3ebee1d91e
Frit027 <Frit_027@mail.ru>
2024-09-20 10:06:09 +0300

    moved to components

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { InputControl } from './components/input-control';
-import {AutocompleteInputViewModel, InputViewModel} from './view-models';
-import { AutocompleteInputControl } from './components/autocomplete-input-control';
-import './common/index.css';
+import { InputControl } from '../input-control';
+import {AutocompleteInputViewModel, InputViewModel} from '../../view-models';
+import { AutocompleteInputControl } from '../autocomplete-input-control';
+import { classes, texts } from './constants';
+import '../../common/index.css';
+import './index.css';
 
 /**
  * Главный компонент приложения
@@ -16,24 +18,24 @@ export const App = () => {
   const autocompleteInputViewModel1 = new AutocompleteInputViewModel();
   const autocompleteInputViewModel2 = new AutocompleteInputViewModel();
 
-  return <div>
-    <h3>Контрол с 2 кнопками справа</h3>
+  return <div className={classes.component}>
+    <h3>{texts.controlWithButtonsRight}</h3>
     <InputControl
       viewModel={inputViewModel1}
       rightButtons={[
         { title: 'Очистить', callback: inputViewModel1.clear },
-        { title: 'Установить Hello world!', callback: () => inputViewModel1.setValue('Hello world!') },
+        { title: 'Установить "Hello world!"', callback: () => inputViewModel1.setValue('Hello world!') },
       ]}
     />
-    <h3>Контрол с 1 кнопкой справа и 1 кнопкой слева</h3>
+    <h3>{texts.controlWithButtonsBothSides}</h3>
     <InputControl
       viewModel={inputViewModel2}
       leftButtons={[{ title: 'alert, если число', callback: inputViewModel2.alertIfNumber }]}
       rightButtons={[{ title: 'alert', callback: inputViewModel2.alertValue }]}
     />
-    <h3>Контрол-автокомплит (макс. кол-во подсказок - 3)</h3>
+    <h3>{texts.autocompleteControl3Tips}</h3>
     <AutocompleteInputControl viewModel={autocompleteInputViewModel1} maxTips={3} />
-    <h3>Контрол-автокомплит (макс. кол-во подсказок - 10)</h3>
+    <h3>{texts.autocompleteControl10Tips}</h3>
     <AutocompleteInputControl viewModel={autocompleteInputViewModel2} maxTips={10} />
   </div>;
 };
