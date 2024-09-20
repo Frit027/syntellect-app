6a36273e40e0dac213f5bf891b318308347893c4
Frit027 <Frit_027@mail.ru>
2024-09-20 09:34:17 +0300

    removed class for input

--- src/components/autocomplete-input-control/constants.ts
+++ src/components/autocomplete-input-control/constants.ts
@@ -1,7 +1,6 @@
 export const classes = {
   component: 'autocomplete-input',
   list: 'autocomplete-input__list',
-  input: 'autocomplete-input__input',
   item: 'autocomplete-input__item',
   text: 'autocomplete-input__text',
   flag: 'autocomplete-input__flag',
