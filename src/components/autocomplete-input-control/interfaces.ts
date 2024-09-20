b57c3e782227a85465fd82b76032824fb41c2f87
Frit027 <Frit_027@mail.ru>
2024-09-20 09:35:20 +0300

    added comment

--- src/components/autocomplete-input-control/interfaces.ts
+++ src/components/autocomplete-input-control/interfaces.ts
@@ -4,6 +4,7 @@ import { AutocompleteInputViewModel } from '../../stores';
  * Тип пропсов контрола-автокомплита
  */
 export type TProps = {
+  // ViewModel для обновления и получения данных
   viewModel: AutocompleteInputViewModel,
 
   // максимальное количество подсказок
