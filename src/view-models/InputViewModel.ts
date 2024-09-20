c7ebd74a16e55778a8bafa956fb8a5d94cefd72f
Frit027 <Frit_027@mail.ru>
2024-09-20 10:27:27 +0300

    added JSDocs

--- src/view-models/InputViewModel.ts
+++ src/view-models/InputViewModel.ts
@@ -1,24 +1,41 @@
 import { makeObservable, observable, action } from 'mobx';
 
+/**
+ * ViewModel для текстового контрола с кнопками
+ */
 export class InputViewModel {
+  // значение инпута
   @observable value = '';
 
   constructor() {
     makeObservable(this);
   }
 
+  /**
+   * Установка нового значения для value
+   * @param {string} newValue - Новое значение
+   */
   @action
   setValue = (newValue: string) => {
     this.value = newValue;
   };
 
+  /**
+   * Очистка value
+   */
   @action
   clear = () => {
     this.value = '';
   };
 
+  /**
+   * Показ сообщения со значением value
+   */
   alertValue = () => alert(this.value);
 
+  /**
+   * Показ сообщения, если значение value является числом
+   */
   alertIfNumber = () => {
     const numberRegExp = /^-?\d+(\.?\d+)?$/;
     if (numberRegExp.test(this.value)) {
