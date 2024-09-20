921bc33d9665f09162ad09b861ca46cbb8c3a243
Frit027 <Frit_027@mail.ru>
2024-09-20 10:33:22 +0300

    added comments

--- src/components/input-control/interfaces.ts
+++ src/components/input-control/interfaces.ts
@@ -1,13 +1,27 @@
 import React from 'react';
 import { InputViewModel } from '../../view-models';
 
+/**
+ * Тип для свойств кнопки
+ */
 type TButton = {
+  // текст кнопки
   title: string,
+
+  // обработчик клика по кнопке
   callback: React.MouseEventHandler<HTMLButtonElement>,
 };
 
+/**
+ * Тип для пропсов компонента
+ */
 export type TProps = {
+  // ViewModel для обновления и получения данных
   viewModel: InputViewModel,
+
+  // свойства для левых кнопок
   leftButtons?: TButton[],
+
+  // свойства для правых кнопок
   rightButtons?: TButton[],
 };
