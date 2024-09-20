20331be8ff7b37af8ce0671f8f6a8a52f704b58b
Frit027 <Frit_027@mail.ru>
2024-09-20 10:05:13 +0300

    created constants for app

---
+++ src/components/app/constants.ts
@@ -0,0 +1,16 @@
+/**
+ * Имена CSS-классов компонента
+ */
+export const classes = {
+  component: 'main-page',
+};
+
+/**
+ * Значения текстовых элементов страницы
+ */
+export const texts = {
+  controlWithButtonsRight: 'Контрол с 2 кнопками справа',
+  controlWithButtonsBothSides: 'Контрол с 1 кнопкой справа и 1 кнопкой слева',
+  autocompleteControl3Tips: 'Контрол-автокомплит (макс. кол-во подсказок - 3)',
+  autocompleteControl10Tips: 'Контрол-автокомплит (макс. кол-во подсказок - 10)',
+};
