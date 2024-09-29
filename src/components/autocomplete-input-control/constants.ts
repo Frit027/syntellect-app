c4dea052c1d8ddb265c594d3877cb2f26f926c5b
Frit027 <Frit_027@mail.ru>
2024-09-29 17:22:06 +0300

    added class names for loader

--- src/components/autocomplete-input-control/constants.ts
+++ src/components/autocomplete-input-control/constants.ts
@@ -4,6 +4,8 @@
 export const classes = {
   component: 'autocomplete-input',
   list: 'autocomplete-input__list',
+  listMinHeight: 'autocomplete-input__list_min-height',
+  loader: 'autocomplete-input__loader',
   item: 'autocomplete-input__item',
   text: 'autocomplete-input__text',
   flag: 'autocomplete-input__flag',
