cf3ef4e8ec563a9343a4c5f1d90c0c7d2370df2c
Frit027 <Frit_027@mail.ru>
2024-09-29 18:12:58 +0300

    created debounce function

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -3,3 +3,19 @@
  * @returns {string} ID в шестнадцатеричной системе счисления
  */
 export const generateID = (): string => Math.random().toString(16).slice(2);
+
+/**
+ * Debounce-функция, которая выполняет только последний колбэк за указанный период времени
+ * @param {() => void} callback - Колбэк, который нужно выполнить
+ * @param {number} [timeout=500] - Время ожидания
+ * @returns {() => void} Функция, выполняемая 1 раз за указанный промежуток времени
+ */
+export function getDebounceFunction(callback: () => void, timeout: number = 500): () => void {
+  let timerId: NodeJS.Timeout | null = null;
+  return function (this: any) {
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+    timerId = setTimeout(() => callback.apply(this), timeout);
+  };
+}
