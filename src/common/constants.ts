557bbf2a3009ec195238cf949a4a66332fa0a5dd
Frit027 <Frit_027@mail.ru>
2024-09-29 17:18:50 +0300

    added loading statuses

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -4,3 +4,12 @@
 export const classes = {
   input: 'input',
 };
+
+/**
+ * Состояние загрузки данных с сервера
+ */
+export enum LoadingStatus {
+  Idle = 'IDLE',
+  Loading = 'LOADING',
+  Error = 'ERROR',
+}
